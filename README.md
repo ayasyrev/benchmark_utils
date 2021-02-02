1f0ef35f23dc53a44a55667ab7a02b184426ac36
ayasyrev <a.yasyrev@gmail.com>
2021-02-02 13:34:48 +0000

    add Ci, tests

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # benchmark_utils
+[![Tests](https://github.com/ayasyrev/benchmark_utils/workflows/Tests/badge.svg)](https://github.com/ayasyrev/benchmark_utils/actions?workflow=Tests)
---
+++ noxfile.py
@@ -0,0 +1,31 @@
+import nox
+from nox.sessions import Session
+from typing import Any
+
+
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
+    ...
+
+
+@nox.session(python=["3.8"])
+def tests(session):
+    session.run("poetry", "install", external=True)
+    # session.run("pytest", external=True)
+
+
+locations = "src", "tests", "noxfile.py"
+
+
+@nox.session(python=["3.8"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
+
+# @nox.session(python=["3.8"])
+# def mypy(session):
+#     session.install("mypy")
+#     args = session.posargs or locations
+#     install_with_constraints(session, "mypy")
+#     session.run("mypy", *args)
