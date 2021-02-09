0904ccc9089846b0dcb13975d2572be6864fb99b
ayasyrev <a.yasyrev@gmail.com>
2021-02-09 09:20:20 +0000

    img_libs first vers

--- benchmark_utils/image_libs.py
+++ benchmark_utils/image_libs.py
@@ -1,32 +1,10 @@
 import importlib
+from abc import ABC
+from typing import Union, List
+
 # from importlib.metadata import version
 # import sys
-
 # import pkg_resources
-
-ACCIMAGE_AVAILABLE = importlib.util.find_spec("accimage") is not None
-CV2_AVAILABLE = importlib.util.find_spec("cv2") is not None
-IMAGEIO_AVAILABLE = importlib.util.find_spec("imageio") is not None
-PYVIPS_AVAILABLE = importlib.util.find_spec("pyvips") is not None
-SKIMAGE_AVAILABLE = importlib.util.find_spec("skimage") is not None
-JPEG4PY_AVAILABLE = importlib.util.find_spec("jpeg4py") is not None
-PIL_AVAILABLE = importlib.util.find_spec("PIL") is not None
-TORCHVISION_AVAILABLE = importlib.util.find_spec("torchvision") is not None
-
-
-# if TORCHVISION_AVAILABLE:
-#     tv_version = version('torchvision')
-#     print(tv_version)
-# else:
-#     print('TV unavailable')
-# def print_package_versions(packages: List[str]) -> None:
-#     package_versions = {"python": sys.version}
-#     for package in packages:
-#         try:
-#             package_versions[package] = pkg_resources.get_distribution(package).version
-#         except pkg_resources.DistributionNotFound:
-#             pass
-#     print(package_versions)
 
 image_packages_to_lib = {"torchvision": 'torchvision',
                          "pillow-simd": 'PIL',
@@ -41,9 +19,43 @@ image_packages_to_lib = {"torchvision": 'torchvision',
 packages  = ["torchvision", "pillow-simd", "pillow", "opencv-python", "jpeg4py", "accimage", "pyvips", "scikit-image", "imageio"]  # noqa E501, E221
 libraries = ["torchvision", "PIL",                   "cv2",           "jpeg4py", "accimage", "pyvips", "skimage",      "imageio"]  # noqa E501
 image_lib_available = [library for library in libraries if importlib.util.find_spec(library) is not None]
-if "torchvision" in image_lib_available:
-    if not TORCHVISION_AVAILABLE:
-        image_lib_available.pop(image_lib_available.index('torchvision'))
+
+
+class ImageLib(ABC):
+    __version__: str = ''
+    installation_type: str = ''
+    package: Union[str, List[str]] = ''
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}: pkg: {self.package}, ver: {self.__version__}"
+
+    def read(self, file_name: str) -> object:
+        raise NotImplementedError
+
+
+class ImageLibPIL(ImageLib):
+    def __init__(self) -> None:
+        super().__init__()
+        if 'PIL' in image_lib_available:
+            from PIL import __version__, Image
+            self.__version__ = __version__
+            self.image_open = Image.open
+        else:
+            self.__version__ = ''
+
+        self.installation_type = 'pip'
+        self.package = 'pillow'
+
+    def read(self, file_name: str) -> object:
+        with open(file_name, 'rb') as file:
+            img = self.image_open(file)
+            return img.convert('RGB')
+
+
+image_libs_dict = {'PIL': ImageLibPIL}
 
 
 class ImageLibs:
@@ -51,11 +63,11 @@ class ImageLibs:
         self.supported = ["torchvision", "PIL", "cv2", "jpeg4py", "accimage", "pyvips", "skimage", "imageio"]
         self.available = [library for library in self.supported if importlib.util.find_spec(library) is not None]
         # self.lib_versions = {lib_name: version(lib_name) for lib_name in self.available}
+        self._libs = {lib_name: image_libs_dict[lib_name]() for lib_name in self.available
+                      if lib_name in image_libs_dict}
+
+    def __getitem__(self, lib_name):
+        return self._libs[lib_name]
 
     def __repr__(self) -> str:
         return f"Image Libs: {', '.join(self.available)}"
-
-
-img_libs = ImageLibs()
-print(img_libs)
-# print(img_libs.lib_versions)
--- tests/test_image_libs.py
+++ tests/test_image_libs.py
@@ -1,4 +1,14 @@
-from benchmark_utils.image_libs import ImageLibs
+import pytest
+
+from benchmark_utils.image_libs import ImageLib, ImageLibs
+
+
+def test_image_lib():
+    image_lib = ImageLib()
+    assert 'ImageLib' in repr(image_lib)
+    assert 'ImageLib' in str(image_lib)
+    with pytest.raises(NotImplementedError):
+        image_lib.read('')
 
 
 def test_image_libs(img_lib):
@@ -6,3 +16,7 @@ def test_image_libs(img_lib):
     assert img_lib in image_libs.available
     assert 'Image Libs' in repr(image_libs)
     assert img_lib in repr(img_lib)
+    assert img_lib in str(img_lib)
+    img_lib_obj = image_libs[img_lib]
+    img_lib_obj.read('tests/test_imgs/test_img_1.JPEG')
+    image_libs[img_lib].read('tests/test_imgs/test_img_2.JPEG')
