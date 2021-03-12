6199ad2f83ee02941bb1abc810457bfccad706c9
ayasyrev <a.yasyrev@gmail.com>
2021-03-12 13:40:26 +0000

    base version

--- benchmark_utils/image_libs/__init__.py
+++
@@ -1,3 +0,0 @@
-from .image_libs_supported import image_libs_supported, ImageLibCfg  # noqa E401
-# from .read_image import image_read_dict
-# from .image_libs import ImageLib, ImageLibs, image_lib_available
