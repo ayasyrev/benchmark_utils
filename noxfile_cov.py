470f24effea06a284de5a7add6b04acc2ab9c98b
ayasyrev <a.yasyrev@gmail.com>
2022-09-21 16:53:06 +0300

    nox coverage

--- noxfile_cov.py
+++ noxfile_cov.py
@@ -6,3 +6,4 @@ def coverage(session) -> None:
     """Upload coverage data."""
     session.install("coverage[toml]", "codecov")
     session.run("coverage", "xml", "--fail-under=0")
+    session.run("codecov", *session.posargs)
