7317407adaef1daa02ea975571b96909cad1b2ae
ayasyrev <a.yasyrev@gmail.com>
2023-01-27 12:27:35 +0300

    drop python 3.7

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.7", "3.10", "3.11"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def tests(session):
     args = session.posargs or ["--cov"]
     session.install(".", "pytest", "pytest-cov", "coverage[toml]")
