d6659b99082b46305494303b89b18fbdd4adf19e
ayasyrev <a.yasyrev@gmail.com>
2023-07-27 14:32:26 +0300

    setup python version, fix for nox

--- noxfile_conda.py
+++ noxfile_conda.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"], venv_backend="mamba")
+@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10", "3.11"], venv_backend="mamba")
 def tests(session):
     args = session.posargs or ["--cov"]
     session.conda_install("--file", "requirements_test.txt")
